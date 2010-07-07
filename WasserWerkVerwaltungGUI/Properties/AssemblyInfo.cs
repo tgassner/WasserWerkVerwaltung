8ee4dfcb2f67ae4e24d7115494b26ed456afeca5
Thomas Gassner <tgassner@users.noreply.github.com>
2010-07-07 22:19:30 +0000

    versionsinfo

--- WasserWerkVerwaltungGUI/Properties/AssemblyInfo.cs
+++ WasserWerkVerwaltungGUI/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Thomas Gassner, M.Sc.")]
 [assembly: AssemblyProduct("WasserWerkVerwaltung")]
-[assembly: AssemblyCopyright("Copyright ©  2008")]
+[assembly: AssemblyCopyright("Copyright ©  2010")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.3.1.0")]
-[assembly: AssemblyFileVersion("1.3.1.0")]
+[assembly: AssemblyVersion("1.3.2.0")]
+[assembly: AssemblyFileVersion("1.3.2.0")]
