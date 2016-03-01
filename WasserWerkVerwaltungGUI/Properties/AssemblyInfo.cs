29f047f16f1c7789535ebc55f54d67fed8d03a91
Thomas Gassner <thomas.gassner@gmx.net>
2016-03-01 13:33:50 +0100

    Setup....

--- WasserWerkVerwaltungGUI/Properties/AssemblyInfo.cs
+++ WasserWerkVerwaltungGUI/Properties/AssemblyInfo.cs
@@ -1,10 +1,7 @@
-﻿using System.Reflection;
-using System.Runtime.CompilerServices;
+﻿using System.Resources;
+using System.Reflection;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
 [assembly: AssemblyTitle("WasserWerkVerwaltung")]
 [assembly: AssemblyDescription("WasserWerkVerwaltung ist eine einfache Software zur Verwaltung eines Kundenstamms und der laufenden Rechnungen eines Wasserwerks.\r\n\r\nWWV wurde für das Wasserwerk Weinberger implementiert.")]
 [assembly: AssemblyConfiguration("")]
@@ -13,21 +10,8 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("9f6f793c-116a-439c-896d-ca6d3ff097f7")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
 [assembly: AssemblyVersion("1.4.0.0")]
 [assembly: AssemblyFileVersion("1.4.0.0")]
+[assembly: NeutralResourcesLanguage("de-AT")]
